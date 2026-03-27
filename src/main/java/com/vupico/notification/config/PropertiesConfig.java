60a0ea3570d71594ce3d1cb68db7d0735189512f
chandra-vupico <chandrashekhar.jani.c@vupico.com>
2026-03-27 16:36:09 +0530

    replaced aws ses sdk implementation to java smtp based implementation

--- src/main/java/com/vupico/notification/config/PropertiesConfig.java
+++ src/main/java/com/vupico/notification/config/PropertiesConfig.java
@@ -4,6 +4,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties({AwsSesProperties.class, NotificationRabbitProperties.class})
+@EnableConfigurationProperties({NotificationRabbitProperties.class})
 public class PropertiesConfig {}
 
